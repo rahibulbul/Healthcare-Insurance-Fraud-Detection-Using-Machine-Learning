34dce1cf48f3a877ee15fe1467a25e1939ed2cef
Rahi Bulbul <rahibulbulrahi@gmail.com>
2024-07-15 12:32:51 +0100

    Header Done
    Header Done

---
+++ healthcare/src/components/main/header/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
+
+const Header = () => {
+  window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+
+  const navLinkss = document.querySelectorAll(".nav-link");
+
+  navLinkss.forEach((navLinks) => {
+    navLinks.addEventListener("click", () => {
+      document.querySelector(".active")?.classList.remove("active");
+      navLinks.classList.add("active");
+    });
+  });
+
+  return (
+    <header className="header">
+      <Link to="/" className="logo-link">
+        LoGo
+      </Link>
+      <ul>
+        <li>
+          <Link to="/" className="nav-link active">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/services" className="nav-link">
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link to="/get-quote" className="nav-link">
+            Get Quote
+          </Link>
+        </li>
+        <li>
+          <Link to="/shop" className="nav-link">
+            Shop
+          </Link>
+        </li>
+        <li>
+          <Link to="/Login" className="nav-link">
+            Login
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
